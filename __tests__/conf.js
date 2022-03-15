155aa64cbb15c040d4c6f0a115665f222e3a0779
Igor <parczynski@gmail.com>
2022-03-16 00:01:30 +0300

    Add undefined option test

--- __tests__/conf.js
+++ __tests__/conf.js
@@ -1,9 +1,17 @@
 
 
 
-test( 'GEO Check', async () => {
+test( 'Configuration read test', async () => {
     
     const { COLOR } = require( 'conf-dot-json' )
     expect( COLOR ).toBe( 'skyblue' )
+    
+})
+
+
+test( 'Missed option test', async () => {
+    
+    const { SIZE } = require( 'conf-dot-json' )
+    expect( SIZE ).toBeUndefined(  )
     
 })
